c502debedcc4cf2b68587cac3c8f9286368cf8a5
wami ikechukwu <wamiikechukwu@gmail.com>
2024-06-14 15:26:06 +0100

    refactor: file rename.

---
+++ src/utils/customAuth/useProviderAuth.ts
@@ -0,0 +1,37 @@
+import { createProvider } from "@/services/ProviderService";
+import { useNavigate } from "react-router-dom";
+import { useLocalStorage } from "../localStorage";
+
+function useProvider() {
+    const navigate = useNavigate()
+    const {removeItem} = useLocalStorage("admin")
+
+
+
+    const useCreateProvider = async (data: any):Promise<any> => {
+        try {
+            const response = await createProvider(data)
+
+            console.log("the response is", response)
+
+        } catch (error: any) {
+
+            if(error?.response?.request.status == 403){
+                // clear session cache, this doesn't clear the user cache, i don't know why
+                // the session cache needs to be clear else, user won't be able to navigate
+                // to sign in
+                removeItem("admin")
+                removeItem("user")
+
+                navigate("/sign-in")
+
+            }
+        }
+    }
+
+    return {
+        useCreateProvider
+    }
+}
+
+export default useProvider
