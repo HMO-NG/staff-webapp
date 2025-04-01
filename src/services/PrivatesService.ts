d7a8a647b41b78a96b137de293a7f67d64ef8d8b
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-04-01 13:18:25 +0100

    added masterlist url from NHIA enrollee service

--- src/services/PrivatesService.ts
+++ src/services/PrivatesService.ts
@@ -2,7 +2,8 @@ import ApiService from "./ApiService"
 
 type Response = {
     message: string,
-    data: any
+    data: any,
+    count:number
 }
 
 export async function createCompanyService(data: any) {
@@ -16,7 +17,55 @@ export async function getCompanyService() {
   return ApiService.fetchData<Response>({
       url: '/privates/company/get',
       method: 'get',
-      // data
   })
 }
+export async function OnboardCompanyEnrolleesService(data: any) {
+  return ApiService.fetchData<{
+      message: string,
+      data: any,
+      total: number
+  }>({
+      url: '/privates/enrollee/company-masterlist',
+      method: 'post',
+      data
+  })
+}
+
+export async function updateClientStatusService(data:any,id:string) {
+  return ApiService.fetchData<Response>({
+      url: `/privates/company/update/status/${id}`,
+      method: 'put',
+      data
+  })
+}
+export async function updateClientService(data:any,id:string) {
+  return ApiService.fetchData<Response>({
+      url: `/privates/company/update/${id}`,
+      method: 'put',
+      data
+  })
+}
+
+
+export async function getPrivateEnrolleeService() {
+  return ApiService.fetchData<Response>({
+      url: '/privates/enrollee/getall',
+      method: 'get',
+  })
+}
+export async function getPrivateEnrolleeByCompanyIdService(id:string) {
+  return ApiService.fetchData<Response>({
+      url: `/privates/enrollee/get/by/client/${id}`,
+      method: 'get',
+  })
+}
+export async function getPrivateProviderService() {
+  return ApiService.fetchData<Response>({
+      url: `/privates/provider/get`,
+      method: 'get',
+  })
+}
+
+
+
 
