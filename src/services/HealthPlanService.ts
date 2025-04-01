9feb4bb720cc598b27817aadea48e0c2c2393f18
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-04-01 13:45:47 +0100

    updated code

--- src/services/HealthPlanService.ts
+++ src/services/HealthPlanService.ts
@@ -162,6 +162,6 @@ export async function UpdateHealthPlanStatusService(id:any,data:any) {
   }>({
       url: `/healthplan/change-status/${id}`,
       method: 'put',
-      data: typeof data === "boolean" ? { disabled_plan: data } : data,
+      data,
   })
 }
