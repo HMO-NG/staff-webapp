e2007c7409736604c14352936f3824ea9f21d9c3
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-04-14 16:38:10 +0100

    added cloudinary endpoints

--- src/services/DoumentService.ts
+++ src/services/DoumentService.ts
@@ -1,4 +1,5 @@
 import ApiService from "./ApiService"
+import axios from 'axios';
 
 type Response = {
     message: string,
@@ -10,10 +11,27 @@ export async function addDocumentService(data:any) {
   return ApiService.fetchData<{
       message: string,
       data: any,
-      total: number
   }>({
       url: `/doc/add`,
       method: 'post',
       data
   })
+}
+export async function uploadRawFilesTocloudinaryService(data:any,cloud_name:string) {
+  return axios.post<{
+      message: string,
+      data: any,
+  }>(
+      `https://api.cloudinary.com/v1_1/${cloud_name}/raw/upload`,
+      data
+  )
+}
+export async function uploadImagesTocloudinaryService(data:any,cloud_name:string) {
+  return axios.post<{
+      message: string,
+      data: any,
+  }>(
+      `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+      data
+  )
 }
